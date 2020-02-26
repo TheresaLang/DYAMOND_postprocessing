21c2c560a1077fd66d5fb003d4da8333c5d576db
TheresaLang <theresa_lang@gmx.net>
2020-02-26 16:52:04 +0100

    Initial commit. Script to calculate net radiaton flux from upward and downward fluxes.

---
+++ net_radiation_flux_calculation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#SBATCH --job-name=net_radiation_calc
+#SBATCH --output=logs/net_radiation_calc-%j.out
+
+#SBATCH --account=mh1126       # Charge resources on this project account
+#SBATCH --partition=compute,compute2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=36     # Specify number of CPUs per task
+#SBATCH --time=08:00:00        # Set a limit on the total run time
+#SBATCH --mem=0                # use all memory on node
+##SBATCH --constraint=256G      # only run on fat memory nodes (needed for NICAM)
+#SBATCH --monitoring=meminfo=10,cpu=5,lustre=5
+##SBATCH --mail-type=FAIL      # Notify user by email in case of job failure
+
+# Script to calculate net radiation fluxes from upward and downward fluxes
+# One job corresponds to the processing of 
+# Script has to be submitted with option --array=0-<Number_of_inputfiles>
+import os
+import numpy as np
+from os.path import join
+import processing_tools as ptools
+
+# load configuration
+config = ptools.config()
+fluxes = ['SDTOA', 'SUTOA', 'STOA']
+# get list of input and output files and options for horizontal interpolation
+infiles, tempfiles, outfiles = ptools.get_netfluxcalculationfilelist(fluxes, **config)
+
+ID = int(os.environ.get('SLURM_ARRAY_TASK_ID', 0)) # ID corresponds to infile
+
+ptools.calc_net_radiation_flux(infiles[ID], tempfiles[ID], outfiles[ID], fluxes)
