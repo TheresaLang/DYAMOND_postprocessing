fc935ab64abdf401ff18561cbc48c3537c9acaf3
TheresaLang <theresa_lang@gmx.net>
2020-10-09 16:21:17 +0200

    fixup! Split processing into pre- and postprocessing. Move scripts to corresponding subfolders and rename some of them

--- get_FV3_gridspec.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-for N in {1..6}; do 
-    cdo import_fv3grid /mnt/lustre02/work/ka1081/DYAMOND/FV3-3.25km/2016081100/grid_spec.tile$N.nc /mnt/lustre02/work/mh1126/m300773/DYAMOND/FV3/FV3-3.25km_gridspec.tile$N.nc
-done
-cdo collgrid /mnt/lustre02/work/mh1126/m300773/DYAMOND/FV3/FV3-3.25km_gridspec.tile?.nc /mnt/lustre02/work/mh1126/m300773/DYAMOND/FV3/FV3-3.25km_gridspec.nc
