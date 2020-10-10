33471fa7e544540bc20e29d83935591715577928
TheresaLang <theresa_lang@gmx.net>
2020-10-10 12:16:59 +0200

    Initial commit
    Functions returning filenames

---
+++ postprocessing/filenames.py
@@ -0,0 +1,17 @@
+def filenames_preprocessed_output():
+    pass
+
+def filenames_random_ind():
+    pass
+
+def filenames_selected_profiles():
+    pass
+
+def filenames_averaged_profiles():
+    pass
+
+def filenames_heightfile():
+    pass
+
+def filenames_landmask():
+    pass
