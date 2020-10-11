385fa4c5d1da2e99c67542ab686ff31918f91d6f
TheresaLang <theresa_lang@gmx.net>
2020-10-11 19:00:27 +0200

    Fix bug in averaged_profiles

--- postprocessing/filenames.py
+++ postprocessing/filenames.py
@@ -56,9 +56,10 @@ def selected_profiles(data_dir, model, run, variable, num_samples, time_period, 
     return filename
     
 
-def averaged_profiles(data_dir, model, run, var, num_samples, time_period, info_timesteps):
+def averaged_profiles(data_dir, model, run, variable, num_samples, time_period, info_timesteps):
     """
     """
+    start_time_str, end_time_str = time_period_str(time_period)
     filename = f'{model}-{run}_{variable}_averages_{num_samples}_{start_time_str}-{end_time_str}.nc'
     filename = join(data_dir, model, 'random_samples', filename)
     return filename
